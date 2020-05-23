6df6d66b400bb584d7a6126e74be64409e7aa4a7
ishantmishra <ishant.mishra.1029@gmail.com>
2020-05-24 00:04:10 +0530

    starting with java script

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log("Hello World!");
+
+var myName = "Ishant"
+
+myName = 8 
+
+let wholeName = "Ishant Mishra"
+
+const pi = 3.14
+
+console.log(wholeName);
+
+var a;
+var b = 2;
+
+a = 7;
+b = a;
+
+console.log(a);
+
+var sum = 10 + 10;
+console.log(sum);
+
+var diference = 45 - 33;
+console.log(diference);
+
+var product = 9 * 8;
+console.log(product);
+
+var quotient = 66 / 33;
+console.log(quotient);
+
+a++;
+a--;
+
+var product = 2.0 * 2.5;
+console.log(product);
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
+
+a += 12;
+b += 15;
+
+a -= 1;
+b -= 6;
+
+a *= 5;
+b *= 3
+
+a /= 2
+b /=2
+
+var myStr = '"I am a "double quoted" string inside "double quotes"'
+console.log(myStr);
