64a79a9b6b62d94b9a4f5ca740fb62b9a4a00701
ishantmishra <ishant.mishra.1029@gmail.com>
2020-05-24 18:36:58 +0530

    js

--- app.js
+++ app.js
@@ -80,3 +80,116 @@ var lastName = "Mishra";
 
 firstLetterOfLastName = lastName[0];
 console.log(firstLetterOfLastName);
+
+console.log(a);
+
+var myStr = "Jello World";
+
+myStr = "Hello World";
+
+var firstName = "Ishant";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+console.log(lastLetterOfFirstName);
+
+function worldBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb
+    return result;
+}
+
+console.log(worldBlanks("dog", "big", "ran", "quickly"));
+
+var myArray = [50,60,70];
+var myData = myArray[0]
+console.log(myData);
+
+myArray[0] = 45;
+console.log(myArray);
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+var myData = myArray[2][1];
+console.log(myData);
+
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+console.log(myArray);
+
+var myArray = [["John", 23], ["cat", 21]];
+var removedFromMyArray = myArray.pop();
+console.log(myArray);
+
+var myArray = [["John", 23], ["cat", 21]];
+var removedFromMyArray = myArray.shift();
+console.log(myArray);
+
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift();
+ourArray.unshift("Happy");
+console.log(ourArray);
+
+var myList = [["cereal", 3], ['milk', 2], ["banana", 3], ["juice", 2]];
+
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
+
+function functionWithArgs(a, b) {
+    console.log(a + b);
+}
+functionWithArgs(2, 2);
+
+var myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5;
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+fun1();
+fun2();
+
+var processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+function nextInLine(arr, item)
+{
+    arr.push(item);
+    return arr.shift();
+}
+
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+
+function trueOrFalse(wasThatTrue)  {
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+}
+
+trueOrFalse (true);
